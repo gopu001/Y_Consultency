86a087f4cc7685903fe71676b1e6ef8d865fe978
gopu/001 <gopunikhil001@gmail.com>
2024-05-09 18:42:37 +0530

    image

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,7 +3,7 @@ import './Services.scss'
 import design from '../../assets/Services/WebDesign.png'
 import development from '../../assets/Services/Development.png'
 
-import logo from '../../assets/Services/logo.png'
+// import logo from '../../assets/Services/logo.png'
 import poster from '../../assets/Services/poster.png'
 import content from '../../assets/Services/contentWriter.png'
 import digital from '../../assets/Services/digitalMarketing.png'
@@ -30,8 +30,8 @@ const serviceData = [
     },
     {
         id:3,
-        
-        img:logo,
+
+        img:development,
         head: "Logo Design",
         desc:"Our creative designers conceptualize unique logos that capture your brand identity, evoking trust and recognition across all channels, leaving a lasting impression and setting you apart in competitive markets.",
         scroll:"Scroll Down",
